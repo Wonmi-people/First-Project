05adf5e87377fddf3878c95c2a067f7ad0d1776c
C-ON <vmfpsel@gmail.com>
2024-01-24 16:20:59 +0900

    bootstrap update

---
+++ src/main/java/com/javajspstudy/javajsp/JavaJspApplication.java
@@ -0,0 +1,13 @@
+package com.javajspstudy.javajsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaJspApplication.class, args);
+    }
+
+}
